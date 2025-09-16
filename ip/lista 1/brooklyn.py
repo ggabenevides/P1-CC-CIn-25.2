bf0e4f57e0550489a8a3f99f49acafd9ed735734
Gabriela Benevides <gabtbenevides@gmail.com>
2025-09-16 11:03:14 -0300

    Update brooklyn.py

---
+++ ip/lista 1/brooklyn.py
@@ -0,0 +1,13 @@
+# coletar dados
+nota_1 = float(input())
+nota_2 = float(input())
+nota_3 = float(input())
+qtde_aulas = int(input())
+qtde_faltas = int(input())
+
+# calcular média e frequência de chris
+media = float((nota_1 + nota_2 + nota_3)/3)
+porcentagem_frequencia = (qtde_faltas/qtde_aulas)*0.01
+
+#resultado
+#requisitos p passar: média (simples) maior ou igual a 7,0 pontos; 75% de frequência nas aulas 
