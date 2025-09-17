51efe2c55050c2a488f440f6384376a67fd89716
Gabriela Benevides <gabtbenevides@gmail.com>
2025-09-17 16:54:23 -0300

    Update apartamento.py

---
+++ ip/lista 1/apartamento.py
@@ -0,0 +1,12 @@
+Número_artigos_lidos_Sheldon = (int(input()))
+Número_artigos_lidos_Leonard = (int(input()))
+Número_artigos_lidos_Raj =  (int(input()))
+Número de artigos lidos por Howard (int(input()))
+
+Número de experimentos realizados por Sheldon (int).
+
+Número de experimentos realizados por Leonard (int).
+
+Número de experimentos realizados por Raj (int).
+
+Número de experimentos realizados por Howard (int).
