e898c6981fd09a7be93aa861baa70b504edfabf9
Gabriela Benevides <gabtbenevides@gmail.com>
2025-09-17 17:04:58 -0300

    Update glavin.py

--- ip/lista 1/glavin.py
+++ ip/lista 1/glavin.py
@@ -1,4 +1,4 @@
-# inciiando a maquina
+# texto inicial
 print("Ativando a máquina...")
 
 #coletar dados 
@@ -10,12 +10,14 @@ nome.capitalize()
 contagem_letra= 0
 for letra in nome:
     contagem_letra += 1
+divisibilidaede = ano%contagem_letra
 
-if ano%contagem_letra == 0:
-    print(f"Previsão confiável! O rebigulador será real em {ano}!")
+#relatorio final
+if divisibilidaede == 0:
+    previsao_confiavel = True
+else 
+    previsao_confiavel = False
+
+if previsao_confiavel == True:
     if nome == "Frink":
-        print("Professor Frink irá inventar o rebigulador?! A máquina deve estar com defeito! Glavin!")
-elif ano%contagem_letra != 0:
-    print(f"Previsão instável! {nome} também deve achar que o rebigulador é ridículo...")
-    if nome == "Frink":
-        print("Nem precisava colocar os dados! O rebigulador jamais existiria em qualquer universo!")
+        
