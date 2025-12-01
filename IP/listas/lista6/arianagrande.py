3daa4fea864178da22e48ecc9258f47800a52d83
ggabenevides <gabtbenevides@gmail.com>
2025-12-01 14:50:39 -0300

    new file:   IP/listas/lista6/arianagrande.py 	modified:   IP/listas/lista6/sabrinacarpenter.py

---
+++ IP/listas/lista6/arianagrande.py
@@ -0,0 +1,76 @@
+entrada = ""
+exigencias = {}
+
+while entrada != "MIMOS RECEBIDOS": # armazenando exigencias da diva
+    entrada = input()
+
+    # tratando dados
+    if entrada != "MIMOS RECEBIDOS":
+        entrada = entrada.split(": ")
+        categoria = entrada[0]
+        item = entrada[1]
+        quantidade = int(entrada[2])
+
+        # caso especial da diva 
+        if item == "latte":
+            quantidade += 1
+
+        # adicionando a biblioteca de exigencias 
+        tupla = (item, quantidade)
+        exigencias[categoria] = tupla
+
+else: # reabastecimento
+    while entrada != "ACABOU, a Glinda está pronta!":
+        entrada = input()
+        if entrada != "ACABOU, a Glinda está pronta!":
+            entrada = entrada.split(" ")
+            categoria_recebida = entrada[5]
+            quantidade_recebida = int(entrada[1])
+            item_recebido = entrada[-2][:-1] 
+
+            # lógica de estoque 
+            flag_chave_existente = exigencias.get(categoria_recebida, "chave inexistente")
+            if flag_chave_existente != "chave inexistente":
+                exigencias.update({categoria_recebida : (item_recebido, quantidade - quantidade_recebida)})
+
+    else:
+        # relatorio final
+        print("Relatório de Balanço Final:")
+        categorias = exigencias.keys()
+        for i in range(len(categorias)):
+            if exigencias[categorias[i]][1] <= 0:
+                frase = "Você entregou TUDO! O mimo tá mais que garantido."
+            else: 
+                frase = f"Golpe BAIXÍSSIMO! Faltam {exigencias[categorias[i]][1]} mimos. Corre!"
+            print(f"Categoria: {categorias[i]} Item: {exigencias[categorias[i]][0]} Status: {frase}")
+        # checagens especificas
+        print()
+
+        flag_maquiagem = exigencias.get("Maquiagem", "chave inexistente")
+        if flag_maquiagem != 'chave inexistente':
+            if exigencias['Maquiagem'][0] == "Gloss":
+                if exigencias["Maquiagem"][1] <= 0:
+                    print("TUDO! O Gloss tá on. O look de Glinda tá salvo!")
+                else:
+                    print("CADÊ meu gloss? Como divarei? ... A Glinda tá chorando de raiva!")
+
+        flag_bebidas = exigencias.get("Bebidas", "chave inexistente")
+        if flag_bebidas != "chave inexistente":
+            if exigencias['Bebidas'][0] == "latte":
+                if exigencias["Bebidas"][1] <= 0:
+                    print("Latte gelado pronto! A voz de Glinda está salva. Pode vir o próximo take")
+                else:
+                    print("Cadeia neles! Faltou o Mimo Sagrado. Essa equipe tá perdida!")   
+
+        # veredito da diva
+        num_estoque_neg = 0
+        print()
+        print("Veredito Final")    
+        for i in range(len(categorias)):
+            if exigencias[categorias[i]][1] > 0:
+                num_estoque_neg += 1 
+
+        if num_estoque_neg >= 3:
+            print("Thank U, Next! A equipe de camarim foi demitida!")
+        else:
+            print("Estoque Aprovado! Glinda vai brilhar em Wicked!")
--- IP/listas/lista6/sabrinacarpenter.py
+++ IP/listas/lista6/sabrinacarpenter.py
@@ -8,17 +8,18 @@ categorias = ("bairro", "quartos", "preço")
 catalogo_phil = {}
 num_propriedades = int(input())
 
+enderecos = []
 for i in range(num_propriedades): # criando catalogo
     informacoes = {} # dicionario interno temporario 
 
     # tratando dados
     dados = input().split(" - ") # [0] = bairro; [1] = endereco-quartos-preco 
-    endereco_quarto_preco = dados[1].split("-")
+    endereco_quarto_preco = dados[2].split("-")
     dados.pop(-1)
-    dados.append(endereco_quarto_preco[0])
-    dados.append(int(endereco_quarto_preco[1]))
-    dados.append(int(endereco_quarto_preco[2])) # CORREÇÃO: [0] = str(bairro); [1] = str(endereco) = chave do catalogo ; [2] = int(quartos); [3] = int(preço)
+    dados.append(int(endereco_quarto_preco[0]))
+    dados.append(int(endereco_quarto_preco[1])) # CORREÇÃO: [0] = str(bairro); [1] = str(endereco) = chave do catalogo ; [2] = int(quartos); [3] = int(preço)
     endereco = dados[1] # vai ser a chave do dicionario principal
+    enderecos.append(endereco)
     dados.pop(1) # CORREÇÃO: [0] = bairro ; [1] = quartos ; [2] = preço
     
     # adicionando dados no dicionario interno do endereço
@@ -33,8 +34,55 @@ print()
 
 # loop de atendimento
 cliente = ""
+
+num_vendas = 0
 while cliente != "FIM":
+    scores = []
+    enderecos_validos = []
     cliente = input()
     if cliente != "FIM":
         requisitos_cliente = tuple(input().split("-"))
-        
+        for i in range(num_propriedades):
+            if (catalogo_phil[enderecos[i]]["quartos"] >= int(requisitos_cliente[0])) and (catalogo_phil[enderecos[i]]["preço"] <= int(requisitos_cliente[1])):
+                # casa válida
+                enderecos_validos.append(enderecos[i])
+                score_total = catalogo_phil[enderecos[i]]["quartos"] * 10 
+                scores.append(score_total)
+        if len(enderecos_validos) == 0:
+            print(f"Puxa, {cliente}, vou te avisar se algo aparecer. Não tenho nada com esses requisitos.")
+            print()
+        else:
+            # escolhendo melhor casa para apresentar
+            maior_score = max(scores)
+            melhor_casa_idx = scores.index(maior_score) # index pega a primeira ocorrencia do maior score na iteracao
+            melhor_casa = enderecos_validos[melhor_casa_idx]
+            # apresentacao da casa
+            print(f"🎤 Bem-vindo ao House Tour de {catalogo_phil[melhor_casa]['bairro']}, {cliente}!")
+            print(f"➡ Casa: {melhor_casa}")
+            print(f"💖 Score: {maior_score} pontos")
+            print()
+            if maior_score >= 40: # casa agradou
+                if cliente == "Taylor Swift":
+                    print('"Essa casa é perfeita para passar as férias na praia!"')
+                elif cliente == "Sabrina Carpenter":
+                    print('"Uau, Phil! Acho que finalmente encontrei o cenário perfeito para o clipe de House Tour!"')   
+                else:
+                    print(f'"{cliente} ficou encantado(a)! Phil comemora mais uma venda de sucesso!"')
+                print()
+                print('Venda concluída! Phil dança triunfante ao som de "House Tour"!')
+                print()  
+                num_vendas += 1              
+            else:
+                if cliente == "Taylor Swift":
+                    print('"Nós nunca vamos comprar essa casa juntos, Phil!"')
+                elif cliente == "Sabrina Carpenter":        
+                    print('"Hmm... Sabe Phil, a letra não era tão literal assim…"')
+                else:        
+                    print('"Parece que a música não ajudou nas vendas dessa vez…"')
+                print()
+                print('Talvez a Sabrina realmente não estivesse falando de imóveis…')
+                print()
+else:
+    print("===== RELATÓRIO DE VENDAS =====")
+    print(f"Total de casas vendidas: {num_vendas}")
+    print("===============================")
